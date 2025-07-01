077a539b1326cbc7f823136f50412f151328a9c6
mannika001 <mannikaverma738@gmail.com>
2025-07-01 21:56:51 +0530

    change in bad email format

--- emailformat.py
+++ emailformat.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import re
 
-data = pd.read_csv("Loglens.LeadLatestWithDaysInStage_171224.csv") 
+data = pd.read_csv("Loglens.csv") 
 
 
 valid_email_regex = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
@@ -14,7 +14,7 @@ def check_email_quality(email):
     email = str(email).strip() # for removing extra space
     if valid_email_regex.match(email): # checking with regex expression
         return 'Good'
-    elif '@' in email and '.' in email.split('@')[-1]: # if @ and . is present or not
+    elif '@' in email: # if @ present but not valid email
         return 'Bad'
     else:
         return 'Poor'
